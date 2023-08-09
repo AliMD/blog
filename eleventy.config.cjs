daea168f91552e17ac5177d757538df9a622c371
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-08-09 19:00:33 +0330

    feat: 11ty config
    Co-authored-by: S. Ali Mihandoost <ali@mihandoost.com>

---
+++ eleventy.config.cjs
@@ -0,0 +1,19 @@
+/**
+ * 11ty configuration.
+ * @param {import("@11ty/eleventy").UserConfig} eleventyConfig
+ * @returns {ReturnType<import("@11ty/eleventy/src/defaultConfig")>}
+ */
+module.exports = function (eleventyConfig) {
+  return {
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    templateFormats: ['njk', '11ty.js'],
+    dir: {
+      input: 'site',
+      output: 'dist',
+      includes: '_includes',
+      data: '_data',
+      layouts: '_layouts',
+    },
+  };
+};
