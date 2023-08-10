5b739d70236686e310a49feaa4e1bba96b14a956
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-08-10 10:58:40 +0330

    feat: minify html

---
+++ config/minify-html.js
@@ -0,0 +1,34 @@
+const swcHtml = require("@swc/html");
+
+/**
+ * @type {import("@swc/html").Options}
+ */
+const swcHtmlOptions = {
+  collapseWhitespaces: "smart",
+  removeEmptyMetadataElements: true,
+  removeComments: true,
+  preserveComments: [],
+  // removeEmptyAttributes: true,
+  // removeRedundantAttributes: true,
+  // collapseBooleanAttributes: true,
+  normalizeAttributes: true,
+  minifyJs: true,
+  minifyCss: true,
+  sortAttributes: true,
+  tagOmission: false,
+  selfClosingVoidElements: true,
+};
+
+async function minifyHtml(content) {
+  if (!this.page.outputPath || !this.page.outputPath.endsWith(".html")) return content;
+  try {
+    const result = await swcHtml.minify(Buffer.from(content), swcHtmlOptions);
+    return result.code;
+  }
+  catch (err) {
+    console.error('minifyHtml Error: ', err);
+    return content;
+  }
+}
+
+module.exports = { minifyHtml };
--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,9 +1,13 @@
+const { minifyHtml } = require('./config/minify-html');
+
 /**
  * 11ty configuration.
  * @param {import("@11ty/eleventy").UserConfig} eleventyConfig
  * @returns {ReturnType<import("@11ty/eleventy/src/defaultConfig")>}
  */
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addTransform('minifyHtml', minifyHtml);
+
   return {
     markdownTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
