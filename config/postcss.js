d96e5c724f486aee830356a171a2666c052a3dcc
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-08-16 18:41:13 +0330

    feat: add tailwindcss

--- config/postcss.js
+++ config/postcss.js
@@ -2,9 +2,11 @@ const postcss = require("postcss");
 const postcssPresetEnv = require("postcss-preset-env");
 const postcssImport = require("postcss-import");
 const cssnano = require("cssnano");
+const tailwindcss = require("tailwindcss");
+const postcssNesting = require('tailwindcss/nesting/index.js');
 
 function postcssProcess(code) {
-  return postcss([postcssPresetEnv, postcssImport, cssnano]).process(code);
+  return postcss([postcssImport, postcssNesting, tailwindcss, postcssPresetEnv, cssnano]).process(code);
 }
 
 module.exports = { postcssProcess };
