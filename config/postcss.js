bb57e0bde92e02fd2ec1b51d55141afc2a37e685
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-08-20 10:41:57 +0330

    feat: change css import root

--- config/postcss.js
+++ config/postcss.js
@@ -3,10 +3,16 @@ const postcssPresetEnv = require("postcss-preset-env");
 const postcssImport = require("postcss-import");
 const cssnano = require("cssnano");
 const tailwindcss = require("tailwindcss");
-const postcssNesting = require('tailwindcss/nesting/index.js');
+const postcssNesting = require("tailwindcss/nesting/index.js");
 
 function postcssProcess(code) {
-  return postcss([postcssImport, postcssNesting, tailwindcss, postcssPresetEnv, cssnano]).process(code);
+  return postcss([
+    postcssImport({ root: "site/_css" }),
+    postcssNesting,
+    tailwindcss,
+    postcssPresetEnv,
+    cssnano,
+  ]).process(code);
 }
 
 module.exports = { postcssProcess };
