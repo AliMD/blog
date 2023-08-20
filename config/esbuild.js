d53c3c41884539e6ed213bbe53d2434e397d6443
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-08-20 14:36:10 +0330

    refactor: use esbuild as filter

--- config/esbuild.js
+++ config/esbuild.js
@@ -1,26 +1,22 @@
-const { build } = require("esbuild");
+const { transform } = require("esbuild");
 const { env } = require("process");
 
 const debugMode = env.NODE_ENV !== "production";
-async function esbuild() {
-  await build({
-    entryPoints: [`site/_js/main.ts`],
-    outdir: "dist/",
+async function esbuildTransform(content) {
+  return (await transform(content, {
     logLevel: "info",
     platform: "browser",
     target: "es2018",
     format: "esm",
-    conditions: debugMode ? ["development"] : undefined,
     minify: true,
-    treeShaking: true,
+    // treeShaking: true,
     sourcemap: true,
     sourcesContent: debugMode,
-    bundle: true,
-    splitting: true,
+    // bundle: true,
+    // splitting: true,
     charset: "utf8",
     legalComments: "none",
-    metafile: true,
-  });
+  })).code;
 }
 
-module.exports = { esbuild };
+module.exports = { esbuildTransform };
