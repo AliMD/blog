79f6655aa81c2d7921dbe8fbc8602f6c828a40f7
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-08-22 08:52:39 +0330

    feat: add register service worker

---
+++ site/_js/register-service-worker.js
@@ -0,0 +1,10 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then(() => {
+      console.log("Service worker registered.");
+    })
+    .catch((error) => {
+      console.log("Registration failed: ", error);
+    });
+}
