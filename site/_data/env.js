6c05304743d7b0cc450e18172dc2bf797a0d02ed
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-09-07 12:37:17 +0330

    feat: update meta

--- site/_data/env.js
+++ site/_data/env.js
@@ -1,7 +1,7 @@
 const env = {
-  // ...process.env,
   siteUrl: process.env.siteUrl ?? '',
   noIndex: process.env.noIndex === '1',
+  production: process.env.NODE_ENV === 'production',
 };
 
 console.log(JSON.stringify(env));
