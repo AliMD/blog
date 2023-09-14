699f7df337289a00906b3d04771d532567f0c72f
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-09-15 01:03:39 +0330

    feat(plugin): add some

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -2,8 +2,13 @@ const {minifyHtml} = require('./config/minify-html');
 const {postcssProcess} = require('./config/postcss.js');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const {esbuildTransform, esbuildBuild} = require('./config/esbuild.js');
+const {date} = require('./config/date.js');
+const {imageShortcode} = require('./shortcode/image.js');
+const {editOnGitHub} = require('./shortcode/edit-on-github.js');
 const directoryOutputPlugin = require('@11ty/eleventy-plugin-directory-output');
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const timeToRead = require('eleventy-plugin-time-to-read');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 /**
  * 11ty configuration.
@@ -18,11 +23,16 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.setQuietMode(true);
 
-  eleventyConfig.addWatchTarget('./site/');
+  eleventyConfig.addWatchTarget('site');
 
   eleventyConfig.on("eleventy.before", esbuildBuild);
 
+  eleventyConfig.addShortcode('image', imageShortcode);
+  eleventyConfig.addShortcode('editOnGitHub', editOnGitHub);
+
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(timeToRead);
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(directoryOutputPlugin, {
     columns: {
@@ -32,6 +42,8 @@ module.exports = function (eleventyConfig) {
     warningFileSize: 400 * 1000,
   });
 
+  eleventyConfig.addFilter('getHostname', getHostname);
+  eleventyConfig.addFilter('humanReadableDate', date);
   eleventyConfig.addAsyncFilter('postcss', postcssProcess);
   eleventyConfig.addAsyncFilter('esbuild', esbuildTransform);
 
