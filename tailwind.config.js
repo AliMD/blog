96c522e4ae07c635ccd78fa4c8a70ea15c86494d
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-09-15 20:38:15 +0330

    fix: set correct file for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,29 @@
-import {tailwindConfig} from '@alwatr/style'
+// import {tailwindConfig} from '@alwatr/style'
 
-export default {
-  ...tailwindConfig,
+import {colorTheme} from '@alwatr/style/lib/colors.js';
+import {elevationPlugin} from '@alwatr/style/lib/elevation.js';
+import {screenTheme} from '@alwatr/style/lib/screen.js';
+import {stateLayerPlugin} from '@alwatr/style/lib/state-layer.js';
+import {typographyTheme} from '@alwatr/style/lib/typography.js';
+import {zIndexTheme} from '@alwatr/style/lib/z-index.js';
+
+const config = {
   content: ['**/*.njk'],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      ...colorTheme,
+      ...typographyTheme,
+      ...zIndexTheme,
+      ...screenTheme,
+    },
+  },
+  plugins: [elevationPlugin, stateLayerPlugin],
 };
+
+// const config = {
+//   ...tailwindConfig,
+//   content: ['**/*.njk'],
+// };
+
+export default config;
