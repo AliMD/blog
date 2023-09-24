acc142e2286ee91bc51a10902e2f91235b6989ba
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-09-24 11:55:33 +0330

    feat(shortcode): add class list to icon

--- shortcode/alwatr-icon.js
+++ shortcode/alwatr-icon.js
@@ -1,9 +1,14 @@
 const {readFileSync} = require('node:fs');
 
-async function loadIcon(iconName) {
+async function loadIcon(iconName, customClass) {
   const icon = readFileSync(`node_modules/@alwatr/icon/svg/${iconName}.svg`, 'utf8');
+  const customClassList = customClass?.split(' ').map((className) => {
+    return `!${className}`;
+  }
+  ).join(' ') || '';
+
   return `
-    <div class="box-content h-[1em] w-[1em] align-middle [contain:size_layout_paint_style] [&>svg]:block [&>svg]:h-full [&>svg]:w-full [&>svg]:fill-current [&>svg]:stroke-current">
+    <div class="inline-block box-content h-[1em] w-[1em] align-middle [contain:size_layout_paint_style] [&>svg]:block [&>svg]:h-full [&>svg]:w-full [&>svg]:fill-current [&>svg]:stroke-current ${customClassList}">
       ${icon}
     </div>
   `;
