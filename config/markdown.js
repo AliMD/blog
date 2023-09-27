5094ba9fd44ca11c4c74192897f1413386583a40
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-09-27 11:10:42 +0330

    fix(markdown): enable html

--- config/markdown.js
+++ config/markdown.js
@@ -2,7 +2,9 @@ const markdownIt = require('markdown-it');
 const markdownItAnchor = require('markdown-it-anchor');
 const markdownItLinkTarget = require('markdown-it-link-attributes');
 
-const markdown = markdownIt();
+const markdown = markdownIt({
+  html: true,
+});
 
 markdown.use(markdownItAnchor);
 markdown.use(markdownItLinkTarget, {
