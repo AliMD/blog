e1744c5651ff1ede507848bb599426b07457a2f5
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-09-27 14:16:56 +0330

    feat(markdown): add slugify

--- config/markdown.js
+++ config/markdown.js
@@ -1,12 +1,15 @@
 const markdownIt = require('markdown-it');
 const markdownItAnchor = require('markdown-it-anchor');
 const markdownItLinkTarget = require('markdown-it-link-attributes');
+const string = require('string');
+
+const slugify = (s) => string(s).slugify().toString();
 
 const markdown = markdownIt({
   html: true,
 });
 
-markdown.use(markdownItAnchor);
+markdown.use(markdownItAnchor, {slugify});
 markdown.use(markdownItLinkTarget, {
   attrs: {
     target: '_blank',
