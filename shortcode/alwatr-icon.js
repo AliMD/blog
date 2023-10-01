78c9c77ca4264f32fdc4bf14494bc3d0d54952d4
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-10-01 12:47:25 +0330

    refactor: use material icon set (#82)
    Co-authored-by: S. Ali Mihandoost  <ali@mihandoost.com>
    Co-authored-by: Abolfazl Rasoulizade <AbolfazlRasoulizade@gmail.com>

--- shortcode/alwatr-icon.js
+++ shortcode/alwatr-icon.js
@@ -1,12 +1,15 @@
 const {readFileSync} = require('node:fs');
 
-async function loadIcon(iconName, customClass = 'h-[1em] w-[1em]') {
-  const icon = readFileSync(`node_modules/@alwatr/icon-set-ionic/svg/${iconName}.svg`, 'utf8');
-  return `
-    <div class="inline-block box-content align-middle [contain:size_layout_paint_style] [&>svg]:block [&>svg]:h-full [&>svg]:w-full [&>svg]:fill-current [&>svg]:stroke-current ${customClass}">
-      ${icon}
-    </div>
-  `;
+async function loadIcon(iconName, customClass = '') {
+  let icon;
+
+  try {
+    icon = readFileSync(`node_modules/@alwatr/icon-set-material/svg/outline/${iconName}.svg`, 'utf8');
+  } catch {
+    icon = 'N!';
+  }
+
+  return `<div class="h-[1em] w-[1em] box-content align-middle self-center grow-0 shrink-0 [contain:size_layout_paint_style] [&>svg]:block [&>svg]:h-full [&>svg]:w-full [&>svg]:fill-current ${customClass}">${icon}</div>`;
 }
 
 module.exports = {loadIcon};
